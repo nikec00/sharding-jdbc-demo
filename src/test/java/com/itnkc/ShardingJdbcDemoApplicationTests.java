e2a6e4f6a73d3dfcd2cab13de14df854fe2b5a9f
Nikecheng <n760144709@163.com>
2022-03-21 22:25:51 +0800

    水平分库

--- src/test/java/com/itnkc/ShardingJdbcDemoApplicationTests.java
+++ src/test/java/com/itnkc/ShardingJdbcDemoApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.WeakHashMap;
 
 @SpringBootTest
 public class ShardingJdbcDemoApplicationTests {
@@ -40,4 +41,20 @@ public class ShardingJdbcDemoApplicationTests {
         System.out.println(courses);
     }
 
+    @Test
+    public void addCourseDb(){
+        Course course = new Course();
+        course.setCname("javaDemo");
+        course.setUserId(101L);
+        course.setCstatus("Normal");
+        int insert = courseDAO.insert(course);
+    }
+
+    @Test
+    public void selectOne(){
+        QueryWrapper<Course> wrapper = new QueryWrapper<>();
+        wrapper.eq("cid",712791348788330497L);
+        Course course = courseDAO.selectOne(wrapper);
+        System.out.println(course);
+    }
 }
